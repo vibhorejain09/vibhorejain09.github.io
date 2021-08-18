5f5e000396188c21457da23c712966c8de134271
vibhorejain09 <vibhorejain0911@gmail.com>
2021-08-18 11:39:18 +0530

    UIbuild

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Components/Header/index';
 import Home from './Components/Home/index';
+import About from './Components/About/index';
 function App() {
   return (
     <>
     <Header />
     <Home />
+    <About />
     </>
   );
 }
